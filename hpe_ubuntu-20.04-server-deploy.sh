61fb43431c1ec3bfc691dc7f1589e7bbb86a9132
mrgs83 <40406835+mrgs83@users.noreply.github.com>
2021-12-11 16:29:52 +0200

    Update hpe_ubuntu-20.04-server-deploy.sh

--- hpe_ubuntu-20.04-server-deploy.sh
+++ hpe_ubuntu-20.04-server-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 apt update && apt upgrade -y;
-apt install gcc make perl curl mc htop zip;
+apt install -y gcc make perl curl mc htop zip;
 mkdir /tmp7204 && cd /tmp7204;
 wget https://raw.githubusercontent.com/mrgs83/HighPoint_SSD7204/main/hptnvme_g5_linux_src_v1.3.2_2021_10_27.bin;
 wget https://raw.githubusercontent.com/mrgs83/HighPoint_SSD7204/main/RAID_Manage_Linux_v3.0.7_21_11_12.bin;
