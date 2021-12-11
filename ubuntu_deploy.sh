59d6961f75b68ec17be996d15ad0fc919af8ddee
mrgs83 <40406835+mrgs83@users.noreply.github.com>
2021-12-11 14:44:45 +0200

    Update and rename ubuntu_deployment.sh to ubuntu_deploy.sh

--- ubuntu_deploy.sh
+++ ubuntu_deploy.sh
@@ -3,4 +3,4 @@ apt update && apt upgrade -y;
 apt install gcc make perl;
 mkdir /tmp7204 && cd /tmp7204;
 wget hptnvme_g5_linux_src_v1.3.2_2021_10_27.bin;
-wget RAID_Manage_Linux_v3.0.7_21_11_12.tgz;
+wget RAID_Manage_Linux_v3.0.7_21_11_12.bin;
