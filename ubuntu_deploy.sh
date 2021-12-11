eac5a16bc16995fc9d009e7e4d9bdb2a327446e9
mrgs83 <40406835+mrgs83@users.noreply.github.com>
2021-12-11 14:49:57 +0200

    Update ubuntu_deploy.sh

--- ubuntu_deploy.sh
+++ ubuntu_deploy.sh
@@ -2,5 +2,10 @@
 apt update && apt upgrade -y;
 apt install gcc make perl;
 mkdir /tmp7204 && cd /tmp7204;
-wget hptnvme_g5_linux_src_v1.3.2_2021_10_27.bin;
-wget https://github.com/mrgs83/HighPoint_SSD7204/blob/main/RAID_Manage_Linux_v3.0.7_21_11_12.bin;
+wget https://raw.githubusercontent.com/mrgs83/HighPoint_SSD7204/main/hptnvme_g5_linux_src_v1.3.2_2021_10_27.bin;
+wget https://raw.githubusercontent.com/mrgs83/HighPoint_SSD7204/main/RAID_Manage_Linux_v3.0.7_21_11_12.bin;
+chmod +x hptnvme_g5_linux_src_v1.3.2_2021_10_27.bin;
+chmod +x RAID_Manage_Linux_v3.0.7_21_11_12.bin;
+./hptnvme_g5_linux_src_v1.3.2_2021_10_27.bin;
+./RAID_Manage_Linux_v3.0.7_21_11_12.bin;
+reboot
